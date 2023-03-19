884555e99960be485855f37c0b0a98bbf2c21fe9
roudra323 <asirroudra@gmail.com>
2023-03-19 18:31:59 +0600

    added card.jsx component

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,20 +1,20 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
 
 export default function Card() {
   return (
     <div className="card">
-      <img
-        src="../src/assets/image 12.png"
-        alt="katie"
-        className="card--image"
-      />
-
+      <img src="../src/assets/image 12.png" className="card--image" />
       <div className="card--stats">
-        <img src="../src/assets/Star.png" />
+        <img src="../src/assets/star.png" className="card--star" />
         <span>5.0</span>
-        <span>(6) • </span>
-        <span>USA</span>
+        <span className="gray">(6) • </span>
+        <span className="gray">USA</span>
       </div>
+      <p>Life Lessons with Katie Zaferes</p>
+      <p>
+        <span className="bold">From $136</span> / person
+      </p>
     </div>
   );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Card from "../components/Card";
 import "./index.css";
 
 {
@@ -10,7 +11,14 @@ import "./index.css";
 function App() {
   return (
     <div>
-      <Navbar />,
+      <Navbar />
+      <Hero />
+      <div className="card-container">
+        <Card className="card" />
+        <Card className="card" />
+        <Card className="card" />
+        <Card className="card" />
+      </div>
     </div>
   );
 }
